ca98250408c9dcc0550d684981fc94a7d9df54b8
Belen Humbert <belenhumbert69@gmail.com>
2024-12-08 00:41:36 -0300

    Nuevos ejercicios ok

---
+++ Modulo 1/M1L4 Proyectos Individuales/mi_historia.py
@@ -0,0 +1,22 @@
+# Para que la historia sea lo más interesante posible, ¡permitiremos que el usuario partícipe en su creación!
+# 
+# En primer lugar, le haremos al usuario entre 5 y 10 preguntas sencillas y guardaremos sus respuestas.
+# 
+# Después de eso, creamos una breve historia divertida (o de ficción) utilizando la información que obtuvimos del usuario. ¡Puedes elegir cualquier tema para la historia que te guste!🚀
+# 
+# Una vez que la historia esté lista, solo tienes que enviarla a la consola y sorprender a todos con lo interesante que ha resultado tu historia.😉
+# 
+# A la izquierda, puedes ver un ejemplo para crear una historia corta de ficción. Puedes ejecutarlo y ver cómo funciona :)
+# 
+# ¡Tu historia puede ser mucho más interesante que esta!
+
+
+
+nombre = input("Introduce tu nombre:")
+animal = input("Introduce el nombre de tu mascota:")
+miedo = input ("¿Qué animal o insecto te desagrada más?")
+planeta = input ("¿Qué nombre le pondrías a tu planeta🪐?")
+galaxia = input ("¿Qué nombre le pondrías a una galaxia🌌?")
+
+print (nombre, ", un joven aventurero, encontró un mapa antiguo en el desván de su abuelo. Junto a su fiel mascota,", animal,", zarparon en busca de una isla misteriosa, donde un legendario tesoro estaba oculto. Durante el viaje, enfrentaron tormentas y criaturas aterradoras, incluyendo gigantescas",miedo,", el mayor miedo de", nombre,
+"que se disfrazan con éxito en la Tierra, haciendo creer a la gente que son gente ordinaria", miedo,".""Cuando llegaron a la isla, descubrieron trampas y secretos antiguos. Sin embargo, otro grupo los perseguía, también buscando el tesoro. Tras una serie de desafíos,", nombre, "y", animal, "lograron llegar a una cueva secreta donde encontraron el tesoro y un artefacto con el poder de controlar los mares." "En un enfrentamiento final,", nombre, "y", animal, "superaron a sus rivales y escaparon con el artefacto. Regresaron como héroes, y", nombre, "supo que su próxima aventura lo llevaría a explorar el espacio, comenzando por", planeta, "y la galaxia", galaxia,".")
---
+++ Modulo 1/M1L4 Proyectos Individuales/realismo.py
@@ -0,0 +1,43 @@
+# Intenta crear y dibujar el dibujo más interesante y genial que puedas, utilizando todas las herramientas de la tortuga que conozcas. ¡No te olvides de la lista de nuevos comandos de las primeras asignaciones!
+# 
+# Recordemos algunos colores interesantes:
+# 
+# 
+# ¡Si tienes alguna dificultad, no dudes en preguntarle a tu instructor! Pero recuerda que ahora tú estás a cargo del proceso :)
+# 
+# ¡Buena suerte!
+
+
+import turtle
+
+t = turtle.Turtle()
+# Escribe tu código a continuación
+
+t.pencolor("white")
+t.color("red")
+t.pencolor("black")
+t.speed(10)
+
+t.fill(1)
+t.rt(45)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.left(45)
+t.fd(40)
+t.left(45)
+t.fd(40)
+t.left(45)
+t.fd(15)
+t.left(45)
+t.fd(40)
+t.rt(90)
+t.fd(40)
+t.left(45)
+t.fd(15)
+t.left(45)
+t.fd(40)
+t.left(45)
+t.fd(42)
+t.left(45)
+t.fill(0)
